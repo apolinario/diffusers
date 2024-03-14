5996237aec9254a4ef0988df54d8058a7235de48
apolinário <joaopaulo.passos@gmail.com>
2024-03-14 11:09:49 -0500

    yield-new-pipe

--- src/diffusers/pipelines/stable_cascade/pipeline_stable_cascade.py
+++ src/diffusers/pipelines/stable_cascade/pipeline_stable_cascade.py
@@ -452,7 +452,7 @@ class StableCascadeDecoderPipeline(DiffusionPipeline):
                 for k in callback_on_step_end_tensor_inputs:
                     callback_kwargs[k] = locals()[k]
                 callback_outputs = callback_on_step_end(self, i, t, callback_kwargs)
-
+                yield callback_outputs
                 latents = callback_outputs.pop("latents", latents)
                 prompt_embeds = callback_outputs.pop("prompt_embeds", prompt_embeds)
                 negative_prompt_embeds = callback_outputs.pop("negative_prompt_embeds", negative_prompt_embeds)
@@ -479,4 +479,4 @@ class StableCascadeDecoderPipeline(DiffusionPipeline):
 
         if not return_dict:
             return images
-        return ImagePipelineOutput(images)
+        yield ImagePipelineOutput(images)
