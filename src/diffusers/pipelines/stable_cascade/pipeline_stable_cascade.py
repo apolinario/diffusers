020fc73418587537d6369c10a9fa985d73bd1e12
apolinário <joaopaulo.passos@gmail.com>
2024-03-14 12:49:59 -0500

    Update pipeline_stable_cascade.py

--- src/diffusers/pipelines/stable_cascade/pipeline_stable_cascade.py
+++ src/diffusers/pipelines/stable_cascade/pipeline_stable_cascade.py
@@ -453,6 +453,7 @@ class StableCascadeDecoderPipeline(DiffusionPipeline):
                     callback_kwargs[k] = locals()[k]
 
                 callback_outputs = callback_on_step_end(self, i, t, callback_kwargs)
+                preview_output = callback_outputs.pop("preview_output", preview_output)
                 yield callback_outputs
                 latents = callback_outputs.pop("latents", latents)
                 prompt_embeds = callback_outputs.pop("prompt_embeds", prompt_embeds)
